18931f61f500346a85f6bc84049743c30dd26399
Aatricks <melis.emilio1@gmail.com>
2025-09-26 16:46:16 +0200

    fix: streamline TLP configuration by removing unnecessary parameters and applications

--- arch-setup.sh
+++ arch-setup.sh
@@ -105,20 +105,6 @@ configure_tlp() {
     sudo mkdir -p /etc/tlp.d
     sudo tee /etc/tlp.d/99-custom.conf >/dev/null <<'EOF'
 # TLP custom config
-CPU_DRIVER_OPMODE_ON_AC="active"
-CPU_DRIVER_OPMODE_ON_BAT="active"
-CPU_SCALING_GOVERNOR_ON_AC="performance"
-CPU_SCALING_GOVERNOR_ON_BAT="powersave"
-CPU_ENERGY_PERF_POLICY_ON_AC="default"
-CPU_ENERGY_PERF_POLICY_ON_BAT="power"
-PLATFORM_PROFILE_ON_AC="performance"
-PLATFORM_PROFILE_ON_BAT="low-power"
-WIFI_PWR_ON_AC="on"
-WIFI_PWR_ON_BAT="on"
-CPU_BOOST_ON_AC=1
-CPU_BOOST_ON_BAT=1
-CPU_HWP_DYN_BOOST_ON_AC=1
-CPU_HWP_DYN_BOOST_ON_BAT=1
 # CPU_SCALING_MIN_FREQ_ON_AC=400000
 # CPU_SCALING_MAX_FREQ_ON_AC=3400000
 # CPU_SCALING_MIN_FREQ_ON_BAT=400000
@@ -131,8 +117,6 @@ CPU_HWP_DYN_BOOST_ON_BAT=1
 # INTEL_GPU_BOOST_FREQ_ON_BAT=600
 PCIE_ASPM_ON_BAT="powersupersave"
 USB_AUTOSUSPEND=1
-RUNTIME_PM_ON_AC="auto"
-RUNTIME_PM_ON_BAT="auto"
 RESTORE_DEVICE_STATE_ON_STARTUP=1
 START_CHARGE_THRESH_BAT0=65
 STOP_CHARGE_THRESH_BAT0=80
@@ -244,7 +228,6 @@ install_apps() {
         visual-studio-code-insiders-bin \
         blackbox-terminal \
         zen-browser-bin \
-        vlc \
         envycontrol \
         flatpak \
         fish \
